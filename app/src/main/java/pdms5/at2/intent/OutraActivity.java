e0717e54d2de8848bc3f4e10ce413541cdb95e6a
yuridias0409 <62430812+yuridias0409@users.noreply.github.com>
2021-04-28 18:29:48 -0300

    First Commit
    Aula 22/04

---
+++ app/src/main/java/pdms5/at2/intent/OutraActivity.java
@@ -0,0 +1,64 @@
+package pdms5.at2.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import pdms5.at2.intent.databinding.ActivityOutraBinding;
+
+public class OutraActivity extends AppCompatActivity {
+    // instância da classe de View Binding
+    private ActivityOutraBinding  activityOutraBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityOutraBinding = ActivityOutraBinding.inflate(getLayoutInflater());
+        setContentView(activityOutraBinding.getRoot());
+
+        // Recebendo parâmetros pela Forma #1
+        Bundle parametroBundle = getIntent().getExtras();
+        if(parametroBundle != null){
+            String parametro = parametroBundle.getString(MainActivity.PARAMETRO, "");
+            activityOutraBinding.recebidoTv.setText(parametro);
+        }
+
+        Log.v(getString(R.string.app_name) + "/" + getLocalClassName(), "onCreate: Iniciando ciclo completo");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(getString(R.string.app_name) + "/" + getLocalClassName(), "onStart: Iniciando ciclo visível");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(getString(R.string.app_name) + "/" + getLocalClassName(), "onResume: Iniciando ciclo foreground");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(getString(R.string.app_name) + "/" + getLocalClassName(), "onPause: Finalizando ciclo foreground");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(getString(R.string.app_name) + "/" + getLocalClassName(), "onStop: Finalizando ciclo visível");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(getString(R.string.app_name) + "/" + getLocalClassName(), "onDestroy: Finalizando ciclo completo");
+    }
+
+    public void onClick(View view){
+        finish(); // Chama na sequência onPause, onStop, onDestroy
+    }
+}
